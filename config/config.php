531c04890b3f6e7ac80618d477c5d8e014918ff0
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-18 13:35:16 +0200

    rempl require par spl autoload

--- config/config.php
+++ config/config.php
@@ -1,6 +1,8 @@
 <?php
 
+function my_autoload ($className) {
+    include __DIR__ . "/../src/models/". $className.".php";
+    include __DIR__ . "/../src/controllers/". $className.".php";
+}
 
-require __DIR__ . "/src/models/Zoo.php";
-require __DIR__ . "/src/models/Animal.php";
-require __DIR__ . "/src/models/AnimalZoo.php";
+spl_autoload_register("my_autoload");
