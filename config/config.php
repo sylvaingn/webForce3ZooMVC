c18dcb0ac3b0a4222e726d074eb5efe8f59b75c8
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-05-18 11:58:10 +0200

    creation

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+
+
+require __DIR__ . "/src/models/Zoo.php";
+require __DIR__ . "/src/models/Animal.php";
+require __DIR__ . "/src/models/AnimalZoo.php";
---
+++ src/models/Zoo.php
@@ -0,0 +1,83 @@
+<?php
+
+
+class Zoo {
+
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $city;
+
+    /**
+     * @var string
+     */
+    public $tableName = "Zoo";
+
+
+    /**
+     * @return int
+     */
+    public function getId(){
+        return $this->id;
+    }
+
+    /**
+     * @param integer $id
+     * @return self
+     */
+    public function setId(int $id){
+
+        $this->id = $id;
+        return $this;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getName(){
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     * @return self
+     */
+    public function setName(string $name){
+
+        $this->name = $name;
+        return $this;
+    }
+
+
+
+    /**
+     * @return string
+     */
+    public function getCity(){
+        return $this->city;
+    }
+
+    /**
+     * @param string $city
+     * @return self
+     */
+    public function setCity(string $city){
+
+        $this->city = $city;
+        return $this;
+    }
+
+
+}
